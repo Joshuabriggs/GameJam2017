7e60f4a07203c525ab2d54765ebb28d27adb6f53
Joshuabriggs <i7679794@bournemouth.ac.uk>
2017-01-20 20:10:31 +0000

    Scripts folders and stuff
    dASA

---
+++ New Unity Project/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Gamemanager : MonoBehaviour {
+
+    [HideInInspector]
+    public StageManager stManager;
+
+	// Use this for initialization
+	void Start () {
+	
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static Gamemanager instance = null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Gamemanager instance already exists");
+        }
+        instance = this;
+    }
+}

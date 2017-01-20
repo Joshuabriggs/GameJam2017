f1f244a96af978beab98bf5a910e87c260d5fba7
MrAlexanderJones <i7627480@bournemouth.ac.uk>
2017-01-20 23:29:44 +0000

    Boopy Doopy Audio Manager
    Added Audio Manager and Started on Menu

---
+++ New Unity Project/Assets/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    bool playingMenu = false;
+    private AudioManager audioManager;
+	// Use this for initialization
+	void Start () {
+        audioManager = AudioManager.instance;
+        if(audioManager == null)
+        {
+            Debug.LogError("No AudiomManager found");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!playingMenu)
+        {
+            audioManager.PlaySound("Music_Menu");
+            audioManager.PlaySound("Enviroment_HeavyRain");
+            playingMenu = true;
+        }
+    }
+}
